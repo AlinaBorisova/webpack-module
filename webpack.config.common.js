2998dcef66117ef67ca410bdb1a517e3381d6465
Алина Борисова <b-alina1999.borisova@yandex.ru>
2025-12-11 16:46:20 +0300

    docs: установлен плагины для css

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 module.exports = {
@@ -26,5 +27,17 @@ module.exports = {
         },
       ],
     }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  
 };
