efc8536da54deeb7ed12867912151f7355573bc1
Алина Борисова <b-alina1999.borisova@yandex.ru>
2025-12-11 16:15:44 +0300

    docs: созданы webpack.config для режимов разработки и заполены правила для HtmlWebpackPlugin

---
+++ webpack.config.common.js
@@ -0,0 +1,15 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: 'index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    clean: true,
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: path.resolve(__dirname, 'public/index.html'),
+  })],
+};
