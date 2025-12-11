23974083dba3a60b176be35ed89f678178cd8b82
Алина Борисова <b-alina1999.borisova@yandex.ru>
2025-12-11 16:26:34 +0300

    docs: добавлен CopyPlugin и настроен. Изменены команды сборки

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -1,15 +1,30 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 const path = require('path');
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
-  entry: 'index.js',
+  entry: './index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[contenthash].js',
     clean: true,
   },
-  plugins: [new HtmlWebpackPlugin({
-    template: path.resolve(__dirname, 'public/index.html'),
-  })],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public/index.html'),
+    }),
+    new CopyPlugin({
+      patterns: [
+        { 
+          from: path.resolve(__dirname, 'public/assets'),
+          to: path.resolve(__dirname, 'dist/assets')
+        },
+        { 
+          from: path.resolve(__dirname, 'public/fonts'),
+          to: path.resolve(__dirname, 'dist/fonts')
+        },
+      ],
+    }),
+  ],
 };
