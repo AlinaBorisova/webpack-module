5eafea9d7f438a51b3012a0c39f7a797789d33bb
Алина Борисова <b-alina1999.borisova@yandex.ru>
2025-12-11 17:09:03 +0300

    docs: добавлен devServer

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,7 +1,13 @@
+const path = require('path');
 const { merge } = require('webpack-merge');
 const commonConfig = require('./webpack.config.common');
 
 module.exports = merge(commonConfig, {
   mode: 'development',
   devtool: 'inline-source-map',
+  devServer: {
+    port: 3000,
+    hot: true,
+    open: true,
+  },
 });
